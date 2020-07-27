665d5742b8df6866d09a920eb1f5a3a73515a1a4
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-07-27 20:01:34 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Mycar car = new Mycar();
+            Console.WriteLine(car.num);
+            Console.WriteLine(car.name);
+
+        }
+    }
+
+    class Mycar
+    {
+        public int num = 6;
+        public string name = "komiyasa"; 
+    }
+}
